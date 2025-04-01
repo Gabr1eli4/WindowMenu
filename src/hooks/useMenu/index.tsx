911557508d8aa65f5e2f84e4ce89bd3326c3918e
Alexandrov Alexandr <a-aleksandrov@asoft21.ru>
2025-04-01 15:05:49 +0300

    chore: added useMenu hook

---
+++ src/hooks/useMenu/index.tsx
@@ -0,0 +1,261 @@
+import { Menu } from '@tauri-apps/api/menu';
+
+export async function useMenu() {
+  const menu = await Menu.new({
+    items: [
+      {
+        id: 'file',
+        text: 'File',
+        action: () => {
+          console.log('File pressed');
+        },
+        items: [
+          {
+            id: 'newFile',
+            text: 'New File',
+            action: () => {
+              console.log('New File pressed');
+            },
+          },
+          {
+            id: 'createFile',
+            text: 'Create File',
+            action: () => {
+              console.log('Create File pressed');
+            },
+          },
+          {
+            id: 'openFile',
+            text: 'Open File',
+            action: () => {
+              console.log('Open File pressed');
+            },
+          },
+          {
+            id: 'openRecent',
+            text: 'Open Recent',
+            action: () => {
+              console.log('Open Recent pressed');
+            },
+            items: [
+              {
+                id: 'more',
+                text: 'More',
+                action: () => {
+                  console.log('More pressed');
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 'edit',
+        text: 'Edit',
+        action: () => {
+          console.log('Edit pressed');
+        },
+        items: [
+          {
+            id: 'editUndo',
+            text: 'Undo',
+            action: () => {
+              console.log('Undo pressed');
+            },
+          },
+          {
+            id: 'editRedo',
+            text: 'Redo',
+            action: () => {
+              console.log('Redo pressed');
+            },
+          },
+          {
+            id: 'editCut',
+            text: 'Cut',
+            action: () => {
+              console.log('Cut pressed');
+            },
+          },
+          {
+            id: 'editCopy',
+            text: 'Copy',
+            action: () => {
+              console.log('Copy pressed');
+            },
+          },
+          {
+            id: 'editFind',
+            text: 'Find',
+            action: () => {
+              console.log('Find pressed');
+            },
+          },
+          {
+            id: 'editReplace',
+            text: 'Replace',
+            action: () => {
+              console.log('Replace pressed');
+            },
+          },
+        ],
+      },
+      {
+        id: 'help',
+        text: 'Help',
+        action: () => {
+          console.log('help pressed');
+        },
+        items: [
+          {
+            id: 'helpWelcome',
+            text: 'Welcome',
+            action: () => {
+              console.log('Welcome pressed');
+            },
+          },
+          {
+            id: 'helpDocumentation',
+            text: 'Documentation',
+            action: () => {
+              console.log('Documentation pressed');
+            },
+          },
+          {
+            id: 'helpAbout',
+            text: 'About',
+            action: () => {
+              console.log('About pressed');
+            },
+          },
+          {
+            id: 'helpLicense',
+            text: 'License',
+            action: () => {
+              console.log('License pressed');
+            },
+          },
+        ],
+      },
+      {
+        id: 'view',
+        text: 'View',
+        action: () => {
+          console.log('view pressed');
+        },
+        items: [
+          {
+            id: 'viewAppearance',
+            text: 'Appearance',
+            action: () => {
+              console.log('Appearance pressed');
+            },
+            items: [
+              {
+                id: 'viewAppearanceFullScreen',
+                text: 'Full Screen',
+                action: () => {
+                  console.log('Full Screen pressed');
+                },
+              },
+              {
+                id: 'viewAppearanceZenMode',
+                text: 'Zen Mode',
+                action: () => {
+                  console.log('Zen Mode pressed');
+                },
+              },
+              {
+                id: 'viewAppearanceCenteredLayout',
+                text: 'Centered Layout',
+                action: () => {
+                  console.log('Centered Layout pressed');
+                },
+              },
+              {
+                id: 'viewAppearanceActivityBarPosition',
+                text: 'Activity Bar Position',
+                action: () => {
+                  console.log('Activity Bar Position pressed');
+                },
+                items: [
+                  {
+                    id: 'viewAppearanceActivityBarPositionTop',
+                    text: 'Top',
+                    action: () => {
+                      console.log('Top pressed');
+                    },
+                  },
+                  {
+                    id: 'viewAppearanceActivityBarPositionLeft',
+                    text: 'Left',
+                    action: () => {
+                      console.log('Left pressed');
+                    },
+                  },
+                  {
+                    id: 'viewAppearanceActivityBarPositionRight',
+                    text: 'Right',
+                    action: () => {
+                      console.log('Right pressed');
+                    },
+                  },
+                  {
+                    id: 'viewAppearanceActivityBarPositionBottom',
+                    text: 'Bottom',
+                    action: () => {
+                      console.log('Bottom pressed');
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 'viewLayout',
+            text: 'Layout',
+            action: () => {
+              console.log('Layout pressed');
+            },
+            items: [
+              {
+                id: 'viewLayoutSplitUp',
+                text: 'Split Up',
+                action: () => {
+                  console.log('Split Up pressed');
+                },
+              },
+              {
+                id: 'viewLayoutSplitDown',
+                text: 'Split Down',
+                action: () => {
+                  console.log('Split Down pressed');
+                },
+              },
+              {
+                id: 'viewLayoutSplitLeft',
+                text: 'Split Left',
+                action: () => {
+                  console.log('Split Left pressed');
+                },
+              },
+              {
+                id: 'viewLayoutSplitRight',
+                text: 'Split Right',
+                action: () => {
+                  console.log('Split Right pressed');
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+
+  // If a window was not created with an explicit menu or had one set explicitly,
+  // this menu will be assigned to it.
+  menu.setAsAppMenu().then((res) => {
+    console.log('menu set success', res);
+  });
+}
